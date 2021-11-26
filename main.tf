cb122862b3ddd9f3049a563ed1c951b214d10367
csh <derek@morethancertified.com>
2021-11-27 00:01:43 +0900

    Managed by Terraform

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ module "compute" {
   version = "1.0.1"
 
   aws_region          = "us-east-1"
-  public_key_material = "public-key-material"
+  public_key_material = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDdCssdi6qwXc6jtxCd/fvuiDRt3mmn7h95Jnj/d2g/4hzBSZJ5uUmtgG7OwI9l1hTAve6sW0tqoM773AZoL+S5gjq3jdXS2ysBJ/j9kNPUkQATfvK/NGGfVQTH4OIUvrHnzUHThAMdqbVJb+V/ZnrWGdrbKjfO+nKarlM7vhXOn4LDwy+najoSZgLcvPDCDZ6bjTfGptd6JL5MDduTW7y5JMiK0Z6/U40ssAfMfN1r58qwvh50Je85aEW2UxwSM2KBTUNP/UR8tO1uwBqBJ+PeQSzJRSGUoeo0mVG8EOO3zsCgtDaRRjKIMUc2TxOEktsA+bTThqi0hayYPMWROuC7 ubuntu@ip-172-31-34-113"
   public_sg           = module.networking.public_sg
   public_subnets      = module.networking.public_subnets
 }
